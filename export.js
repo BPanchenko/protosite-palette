0154afa5e13f1129259c80b0bde710032fe2970a
Boris Panchenko <oberon@list.ru>
2022-07-06 09:18:08 +0300

    disabled creation of the `colors.aco` file

--- export.js
+++ export.js
@@ -1,34 +1,37 @@
 import { colors } from './index.js'
-import { createAcoFile } from "adobe-aco"
 import fs from 'fs'
 import kebabCase from "lodash/kebabCase.js"
 import path from 'path'
-import { saveAs } from "file-saver"
-import startCase from "lodash/startCase.js"
 
-const PREFIX = '--clr-'
-var distFile
+// import { createAcoFile } from "adobe-aco"
+// import { saveAs } from "file-saver"
+// import startCase from "lodash/startCase.js"
 
+{
+	const distFile = path.resolve('./assets/colors.css')
+	const PREFIX = '--clr-'
 
-distFile = path.resolve('./assets/colors.css')
-
-fs.truncateSync(distFile, 0)
-fs.appendFileSync(distFile, ':root {')
-colors.forEach(
-	(color, name) => fs.appendFileSync(
-		distFile,
-		`\n\t${PREFIX}${kebabCase(name)}: ${color.hex()};`
+	fs.truncateSync(distFile, 0)
+	fs.appendFileSync(distFile, ':root {')
+	colors.forEach(
+		(color, name) => fs.appendFileSync(
+			distFile,
+			`\n\t${PREFIX}${kebabCase(name)}: ${color.hex()};`
+		)
 	)
-)
-fs.appendFileSync(distFile, '\n}')
-console.log(`${distFile} was updated`)
+	fs.appendFileSync(distFile, '\n}')
 
+	console.log(`${distFile} was updated`)
+}
 
-distFile = path.resolve('./assets/colors.aco')
-
-const acoSwatches = Array.from(colors).map(
-	([name, color]) => [color.rgb(), 'rgb', startCase(name)]
-)
-const acoFile = createAcoFile(acoSwatches) // there are problems from using package `adobe-aco`
-
-saveAs(new Blob([acoFile]), distFile)
+{
+	/*
+	// there are problems from using package `adobe-aco`
+	const distFile = path.resolve('./assets/colors.aco')
+	const acoSwatches = Array.from(colors).map(
+		([name, color]) => [color.rgb(), 'rgb', startCase(name)]
+	)
+	const acoFile = createAcoFile(acoSwatches)
+	saveAs(new Blob([acoFile]), distFile)
+	*/
+}
