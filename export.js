2c13b8394f427bf878daa9cbf5f362d27d67a1cb
Boris Panchenko <oberon@list.ru>
2022-07-06 09:25:16 +0300

    `index.js` file deleted, code copied to file `export.js`

--- export.js
+++ export.js
@@ -1,11 +1,33 @@
-import { colors } from './index.js'
 import fs from 'fs'
 import kebabCase from "lodash/kebabCase.js"
 import path from 'path'
+import chroma from "chroma-js"
+import fs from "fs"
+import toPairs from "lodash/toPairs.js"
 
 // import { createAcoFile } from "adobe-aco"
 // import { saveAs } from "file-saver"
 // import startCase from "lodash/startCase.js"
+
+const rawdata = fs.readFileSync("./colors.json")
+const sourceColors = JSON.parse(rawdata)
+const colors = new Map()
+const palette = new Map()
+
+toPairs(sourceColors).forEach(p => {
+	let name = p[0]
+	let value = p[1]
+	colors.set(
+		name,
+		chroma(value)
+	)
+	palette.set(
+		name,
+		{
+			color: chroma(value)
+		}
+	)
+})
 
 {
 	const distFile = path.resolve('./assets/colors.css')
