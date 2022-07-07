6e5f8934cd22920dcc54f1ceae43d31f2eb02a47
Boris Panchenko <oberon@list.ru>
2022-07-07 23:45:05 +0300

    init the color tones

--- src/utils.js
+++ src/utils.js
@@ -42,9 +42,13 @@ function getColorData(
 	}
 
 	if (withTones) {
-		const step = 0.9 / countTones
-		const func = correlationFunctions[tonesCorrelation]
-		data.tones = (new Array(countTones)).fill(1).map((_, index) => func(step * ++index))
+		const step = 2.8 / countTones
+		const getFactor = correlationFunctions[tonesCorrelation]
+		data.tones = (new Array(countTones)).fill(1).map((_, index) => {
+			const factor = getFactor(step * ++index)
+			const tone = color.saturate(factor).brighten(factor)
+			return getColorData(tone)
+		})
 	}
 	
 	Object.freeze(data)
