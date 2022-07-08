0cf283d95f2ef345609f5141af5f4bf0978db19a
Boris Panchenko <oberon@list.ru>
2022-07-08 09:49:37 +0300

    init function `buildPalette`

--- export.js
+++ export.js
@@ -1,30 +1,16 @@
+import buildPalette from '../index.js'
 import fs from 'fs'
-import { getColorData } from './src/utils.js'
 import kebabCase from 'lodash/kebabCase.js'
 import path from 'path'
 import startCase from 'lodash/startCase.js'
-import toPairs from 'lodash/toPairs.js'
 
 // import { createAcoFile } from 'adobe-aco'
 // import { saveAs } from 'file-saver'
 
 const rawdata = fs.readFileSync('./src/colors.json')
-const sourceColors = JSON.parse(rawdata)
-const colors = new Map()
-const palette = new Map()
+const colors = JSON.parse(rawdata)
+const palette = buildPalette(colors)
 
-toPairs(sourceColors).forEach(p => {
-	let name = p[0]
-	let value = p[1]
-	colors.set(
-		name,
-		getColorData(value)
-	)
-	palette.set(
-		name,
-		getColorData(value, true)
-	)
-})
 
 {
 	const FILE = path.resolve('./assets/colors.css')
@@ -32,7 +18,7 @@ toPairs(sourceColors).forEach(p => {
 
 	fs.truncateSync(FILE, 0)
 	fs.appendFileSync(FILE, ':root {')
-	colors.forEach(
+	palette.forEach(
 		(color, name) => fs.appendFileSync(
 			FILE,
 			`\n\t${PREFIX}${kebabCase(name)}: ${color.hex};`
---
+++ index.js
@@ -0,0 +1,17 @@
+import { getColorData } from './src/utils.js'
+import toPairs from 'lodash/toPairs.js'
+
+export default function buildPalette(colors, config) {
+	const palette = new Map()
+	
+	toPairs(colors).forEach(p => {
+		let name = p[0]
+		let value = p[1]
+		palette.set(
+			name,
+			getColorData(value, true)
+		)
+	})
+
+	return palette
+}
