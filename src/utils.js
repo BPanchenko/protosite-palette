88c31827d3da3fc73a39c90b56f9ae0cb813d9d3
Boris Panchenko <oberon@list.ru>
2022-08-05 20:21:02 +0300

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,15 @@
 import {
-	accentCorrelationByDefault,
-	toneCorrelationByDefault
-} from './constants.js'
+	accentKeys,
+	toneKeys
+} from "./constants.js"
 
 import chroma from "chroma-js"
+import isFunction from "lodash/isFunction.js"
 
 function getColorData(
 	value,
-	toneFn = toneCorrelationByDefault,
-	accentFn = accentCorrelationByDefault
+	toneFn,
+	accentFn
 ) {
 	const color = chroma(value)
 	const data = {
@@ -17,13 +18,21 @@ function getColorData(
 		hsl: color.hsl()
 	}
 
-	if (withTones) {
-		const step = 2.8 / countTones
-		const getFactor = correlationFunctions[tonesCorrelation]
-		data.tones = (new Array(countTones)).fill(1).map((_, index) => {
-			const factor = getFactor(step * ++index)
-			const tone = color.saturate(factor).brighten(factor)
-			return getColorData(tone)
+	if (isFunction(accentFn)) {
+		const step = 2.8 / accentKeys.length
+		data.accents = accentKeys.map((_, index) => {
+			const factor = accentFn(step * ++index)
+			// TODO: calc tint
+			return getColorData(tint)
+		})
+	}
+
+	if (isFunction(toneFn)) {
+		const step = 2.8 / toneKeys.length
+		data.tones = toneKeys.map((_, index) => {
+			const factor = toneFn(step * ++index)
+			const tint = color.saturate(factor).brighten(factor)
+			return getColorData(tint)
 		})
 	}
 	
