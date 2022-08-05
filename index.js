8c8d19a12eb528f64ec8847b83c8fae837acf7b4
Boris Panchenko <oberon@list.ru>
2022-08-05 20:20:27 +0300

    codestyle

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import {
 	accentCorrelationByDefault,
 	toneCorrelationByDefault
-} from './src/constants.js'
+} from "./src/constants.js"
 
-import { getColorData } from './src/utils.js'
-import toPairs from 'lodash/toPairs.js'
+import { getColorData } from "./src/utils.js"
+import toPairs from "lodash/toPairs.js"
 
 export default function getPalette(
 	colors,
