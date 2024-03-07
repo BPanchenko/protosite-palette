c445a52b7b8e99f7e7260f37549aa2f407d6376d
Boris Panchenko <oberon@list.ru>
2024-03-07 21:15:35 +0300

    refactoring

--- builder/build.js
+++ builder/build.js
@@ -1,58 +1,22 @@
 import forOwn from "lodash/forOwn.js"
-import isEmpty from "lodash/isEmpty.js"
 import path from "path"
-import yargs from "yargs"
-import yargsHelpers from "yargs/helpers"
-import readDir from "read-directory"
+
+import { getFlattenPalette, getPalette } from "../index.js"
+import { distPath, sourceFiles } from "./settings.js"
 
 import createACO from "./.aco.js"
-import { getFlattenPalette, getPalette } from "../index.js"
-import { logError } from "./logger.js"
-
-const argv = yargs(yargsHelpers.hideBin(process.argv))
-	.option("source-name", {
-		alias: "name",
-		describe: "Source file name or palette name",
-		type: "string",
-	})
-	.option("out-dir", {
-		alias: "dist",
-		default: process.env.npm_config_out_dir,
-		describe: "Destination directory for the build files",
-		type: "string",
-	}).argv
-
-const sourcePath = path.resolve(
-	process.cwd(),
-	process.env.npm_config_source_dir || "source",
-)
-
-const sourceFiles = (function ({ sourceName, sourcePath }) {
-	try {
-		return readDir.sync(sourcePath, {
-			filter: `${sourceName || "*"}.json`,
-		})
-	} catch (err) {
-		logError(err)
-	}
-})({
-	...argv,
-	sourcePath,
-})
-
-if (isEmpty(sourceFiles)) {
-	logError("Source files not found")
-}
 
 // Build assets
 
 forOwn(sourceFiles, (data, file, sources) => {
 	const { colors, correlations } = JSON.parse(data)
 	const paletteName = path.parse(file).name
-	const distFolder = Object.keys(sources).length > 1 ? paletteName : "."
-	const distPath = path.resolve(process.cwd(), argv.outDir, distFolder)
+	const dist = path.resolve(
+		distPath,
+		Object.keys(sources).length > 1 ? paletteName : ".",
+	)
 	const palette = getPalette(colors, correlations.tone, correlations.accent)
 	const flattenPalette = getFlattenPalette(palette)
 
-	createACO(palette, flattenPalette, distPath)
+	createACO(palette, flattenPalette, dist)
 })
---
+++ builder/settings.js
@@ -0,0 +1,45 @@
+import { logError } from "./logger.js"
+import isEmpty from "lodash/isEmpty.js"
+import path from "path"
+import readDir from "read-directory"
+import yargs from "yargs"
+import yargsHelpers from "yargs/helpers"
+
+const { outDir, sourceName } = yargs(yargsHelpers.hideBin(process.argv))
+	.option("source-name", {
+		alias: "name",
+		describe: "Source file name or palette name",
+		type: "string",
+	})
+	.option("out-dir", {
+		alias: "dist",
+		default: process.env.npm_config_out_dir,
+		describe: "Destination directory for the build files",
+		type: "string",
+	}).argv
+
+const distPath = path.resolve(process.cwd(), outDir)
+
+const sourcePath = path.resolve(
+	process.cwd(),
+	process.env.npm_config_source_dir || "source",
+)
+
+const sourceFiles = (function ({ sourceName, sourcePath }) {
+	try {
+		const files = readDir.sync(sourcePath, {
+			filter: `${sourceName || "*"}.json`,
+		})
+		if (isEmpty(files)) {
+			throw new Error("Source files not found")
+		}
+		return files
+	} catch (err) {
+		logError(err)
+	}
+})({
+	sourceName,
+	sourcePath,
+})
+
+export { distPath, sourceFiles }
