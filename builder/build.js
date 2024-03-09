dc4b1e63ee951dcabb8f81ce050e81bf74d6c17a
Boris Panchenko <oberon@list.ru>
2024-03-09 13:34:18 +0300

    fix

--- builder/build.js
+++ builder/build.js
@@ -1,22 +1,46 @@
-import forOwn from "lodash/forOwn.js"
-import path from "path"
+import camelCase from "lodash/camelCase.js";
+import curry from "lodash/curry.js";
+import forOwn from "lodash/forOwn.js";
+import toPairs from "lodash/toPairs";
 
-import { getFlattenPalette, getPalette } from "../index.js"
-import { distPath, sourceFiles } from "./settings.js"
+import path from "path";
+import saveAs from "file-saver";
 
-import createACO from "./.aco.js"
+import { distPath, sources } from "./settings.js";
+import { getFlattenPalette, getPalette } from "../index.js";
+import createAcoFileContent from "../lib/fn.createAcoFileContent.js";
 
-// Build assets
+const getDestinationPath = curry((hasMultipleSources, root, paletteFolder) =>
+  path.resolve(root, hasMultipleSources ? paletteFolder : ".")
+)(Object.keys(sources).length > 1)(distPath);
 
+/**
+ *
+ * @param {string} row
+ * @returns Array<[string,string]>
+ */
+const getPrimaryColorsFromSourceJSON = (row) =>
+  toPairs(JSON.parse(row).colors).map(([key, hex]) => [camelCase(key), hex]);
+
+// Primary Colors
+
+forOwn(sources, (source, file, sources) => {
+  const data = getPrimaryColorsFromSourceJSON(source);
+  const paletteName = path.parse(file).name;
+  console.log(paletteName, data);
+});
+
+/*
 forOwn(sourceFiles, (data, file, sources) => {
-	const { colors, correlations } = JSON.parse(data)
-	const paletteName = path.parse(file).name
+	const { colors, correlations } = JSON.parse(data);
+	const paletteName = path.parse(file).name;
 	const dist = path.resolve(
-		distPath,
-		Object.keys(sources).length > 1 ? paletteName : ".",
-	)
-	const palette = getPalette(colors, correlations.tone, correlations.accent)
-	const flattenPalette = getFlattenPalette(palette)
+	  distPath,
+	  Object.keys(sources).length > 1 ? paletteName : "."
+	);
+	const palette = getPalette(colors, correlations.tone, correlations.accent);
+  
+	// const flattenPalette = getFlattenPalette(palette)
+  });
 
-	createACO(palette, flattenPalette, dist)
-})
+  */
