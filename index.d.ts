4810b727f13ea4e364b0bd8e97a81280e5f208d3
Boris Panchenko <oberon@list.ru>
2024-03-27 00:54:42 +0300

    define type `Config` and refactoring of other types

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
-import type { ArrayIndices, ArrayValues } from "type-fest"
-import { accentKeys, toneKeys } from "./lib/settings.js"
+import type { ArrayIndices, ArrayValues } from "type-fest";
+import { accentKeys, toneKeys } from "./lib/settings.js";
 
 type CamelCase<S extends string> = S extends `${infer P1}-${infer P2}`
-	? `${Lowercase<P1>}${Capitalize<P2>}`
-	: Lowercase<S>
+  ? `${Lowercase<P1>}${Capitalize<P2>}`
+  : Lowercase<S>;
 
-export type PaletteColorData = ColorData & {
-	accents?: Record<AccentColorKey, ColorData>;
-	tones?: Record<ToneKey, ColorData>;
-}
+export type AccentCorFn = (
+  index: ArrayIndices<typeof accentKeys>
+) => [number, number];
+
+export type ToneCorFn = (index: ArrayIndices<typeof toneKeys>) => number;
 
 export type ColorKey =
   | "red"
@@ -27,37 +28,44 @@ export type ColorKey =
   | "amber"
   | "orange"
   | "deep-orange"
-  | "brown"
-  | "grey"
-  | "blue-grey"
-  | "green-grey"
+  | "brown";
 
 export type ColorName = {
-	[K in ColorKey]: CamelCase<K>;
-}[ColorKey]
+  [K in ColorKey]: CamelCase<K>;
+}[ColorKey];
 
 export type ColorData = {
-	hsl: ColorValue;
-	hex: ColorValue;
-	rgb: ColorValue;
-}
+  hsl: ColorValue;
+  hex: ColorValue;
+  rgb: ColorValue;
+};
 
-export type ColorValue = string
-export type AccentColorKey = ArrayValues<typeof accentKeys>
-export type ToneKey = ArrayValues<typeof toneKeys>
+export type ColorValue = string;
 
-export type AccentCorrelationFunction = (
-	index: ArrayIndices<typeof accentKeys>
-) => [number, number]
+export type Config = {
+  colors: Record<ColorKey, ColorValue>;
+  correlations: {
+    accent: AccentCorFn | [number, number, number, number];
+    tone: ToneCorFn | [number, number, number, number];
+  };
+  format: "hex" | "hsl" | "rgba";
+  mixins: Array<[ColorKey, ColorKey]>;
+  name: string;
+  theme: "dark" | "light";
+};
 
-export type ToneCorrelationFunction = (
-	index: ArrayIndices<typeof toneKeys>
-) => number
+export type PaletteColorData = ColorData & {
+  accents?: Record<AccentColorKey, ColorData>;
+  tones?: Record<ToneKey, ColorData>;
+};
+
+export type AccentColorKey = ArrayValues<typeof accentKeys>;
+export type ToneKey = ArrayValues<typeof toneKeys>;
 
 export type Source = {
-	correlations: {
-		accent: AccentCorrelationFunction | [number, number, number, number];
-		tone: ToneCorrelationFunction | [number, number, number, number];
-	};
-	data: Record<ColorKey, ColorValue>;
-}
+  colors: Record<ColorKey, ColorValue>;
+  correlations: {
+    accent: AccentCorFn | [number, number, number, number];
+    tone: ToneCorFn | [number, number, number, number];
+  };
+};
