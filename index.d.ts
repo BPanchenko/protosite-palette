e10f36e667f5dc339e7ac3920386cd9af32fe2f1
Boris Panchenko <oberon@list.ru>
2024-04-25 00:58:22 +0300

    codestyle and eslint rules

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,16 @@
-import type { ArrayIndices, ArrayValues } from 'type-fest';
-import { accentKeys, toneKeys } from './lib/settings.js';
+import type { ArrayIndices, ArrayValues } from 'type-fest'
+import { accentKeys, toneKeys } from './lib/settings.js'
 
-type CamelCase<S extends string> = S extends `${infer P1}-${infer P2}` ? `${Lowercase<P1>}${Capitalize<P2>}` : Lowercase<S>;
+type CamelCase<S extends string> = S extends `${infer P1}-${infer P2}`
+	? `${Lowercase<P1>}${Capitalize<P2>}`
+	: Lowercase<S>
 
-export type AccentKey = ArrayValues<typeof accentKeys>;
-export type AccentCorFn = (index: ArrayIndices<typeof accentKeys>) => [number, number];
-export type ToneCorFn = (index: ArrayIndices<typeof toneKeys>) => number;
-export type ToneKey = ArrayValues<typeof toneKeys>;
+export type AccentKey = ArrayValues<typeof accentKeys>
+export type AccentCorFn = (
+	index: ArrayIndices<typeof accentKeys>
+) => [number, number]
+export type ToneCorFn = (index: ArrayIndices<typeof toneKeys>) => number
+export type ToneKey = ArrayValues<typeof toneKeys>
 
 export type ColorKey =
 	| 'red'
@@ -26,39 +30,43 @@ export type ColorKey =
 	| 'orange'
 	| 'deep-orange'
 	| 'brown'
-	| 'gray';
+	| 'gray'
 
 export type ColorName = {
-	[K in ColorKey]: K extends 'gray' ? CamelCase<K> : CamelCase<K> | CamelCase<`${K}-gray`>;
-}[ColorKey];
+	[K in ColorKey]: K extends 'gray'
+		? CamelCase<K>
+		: CamelCase<K> | CamelCase<`${K}-gray`>
+}[ColorKey]
 
 export type ColorData = {
-	hsl: ColorValue;
-	hex: ColorValue;
-	rgb: ColorValue;
-};
-
-export type ColorValue = string;
-
-export interface Config {
-	admixturesToGray?: ColorKey;
-	colors: Record<ColorKey, ColorValue>;
-	correlations: {
-		accent: AccentCorFn | [number, number, number, number];
-		tone: ToneCorFn | [number, number, number, number];
-	};
-	name: string;
-	theme: 'dark' | 'light' | ThemeConfig;
+	hsl: ColorValue
+	hex: ColorValue
+	rgb: ColorValue
 }
 
-export interface SourceConfig extends Optional<Omit<Config, 'colors' | 'correlations'>>, Pick<Config, 'colors' | 'correlations'> {}
+export type ColorValue = string
+
+export interface Config {
+	admixturesToGray?: ColorKey
+	colors: Record<ColorKey, ColorValue>
+	correlations: {
+		accent: AccentCorFn | [number, number, number, number]
+		tone: ToneCorFn | [number, number, number, number]
+	}
+	name: string
+	theme: 'dark' | 'light' | ThemeConfig
+}
+
+export interface SourceConfig
+	extends Required<Pick<Config, 'colors' | 'correlations'>>,
+		Optional<Omit<Config, 'colors' | 'correlations'>> {}
 
 export type PaletteColorData = ColorData & {
-	accents?: Record<AccentKey, ColorData>;
-	tones?: Record<ToneKey, ColorData>;
-};
+	accents?: Record<AccentKey, ColorData>
+	tones?: Record<ToneKey, ColorData>
+}
 
 export type ThemeConfig = {
-	background: ColorValue;
-	foreground: ColorValue;
-};
+	background: ColorValue
+	foreground: ColorValue
+}
