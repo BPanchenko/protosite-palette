831856c6d9f92733a4aa8a0684d8626d69000ea2
Boris Panchenko <oberon@list.ru>
2024-04-25 00:59:08 +0300

    fix

--- builder/build.js
+++ builder/build.js
@@ -1,6 +1,12 @@
 import { logError, logSuccess, logger } from './logger.js'
-import { makeAcoContent, makeCssContent, makeDtsContent, makeEsmContent } from '../lib/makeFileContent.js'
+import {
+	makeAcoContent,
+	makeCssContent,
+	makeDtsContent,
+	makeEsmContent
+} from '../lib/makeFileContent.js'
 
+import { capitalize } from 'lodash/camelCase'
 import getPelette from '../lib/fn.getPalette.js'
 import { getPrimaryColors } from '../lib/utils.js'
 import path from 'path'
@@ -8,31 +14,26 @@ import { writeFile } from 'node:fs'
 
 const { name } = path.parse(file)
 const outDir = getDestinationPath(name)
+const palette = getPelette()
 
-logger.debug(`Saving the ${name} palette to "${outDir}"`);
+logger.debug(`Saving the ${name} palette to "${outDir}"`)
+;[
+	['colors', palette.primaryColors],
+	['palette', palette]
+].forEach(([dataName, data]) => {
+	const entries = Object.entries(data)
 
-[
-	['colors', getPrimaryColors()],
-	['palette', getPelette()],
-].forEach(([name, data]) => {
-	const entries = Object.entries(data);
-
-	// Write all colors to files
-
-	[
-		['.css', makeCssContent],
-		['.d.ts', makeDtsContent],
-		['.js', makeEsmContent],
-	].forEach(([ext, fn]) =>
-		writeFile(path.join(outDir, name + ext), Buffer.from(fn(entries)), 'utf8', err => (err ? logError(err) : logSuccess(name + ext))),
-	)
-
-	// Exclude colors with alpha channel for ACO files
-
-	writeFile(
-		path.join(outDir, name + '.aco'),
-		Buffer.from(makeAcoContent(entries.filter(entry => entry[0].includes('TP') === false))),
-		'utf8',
-		err => (err ? logError(err) : logSuccess(name + '.aco')),
+	;[
+		[`${capitalize([palette.name, dataName].join('-'))}.aco`, makeAcoContent],
+		[`${dataName}.css`, makeCssContent],
+		[`${dataName}.d.ts`, makeDtsContent],
+		[`${dataName}.js`, makeEsmContent]
+	].forEach(([file, makeContent]) =>
+		writeFile(
+			path.join(outDir, file),
+			Buffer.from(makeContent(entries)),
+			'utf8',
+			(err) => (err ? logError(err) : logSuccess(file))
+		)
 	)
 })
