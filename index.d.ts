87b8b91936b809fe46a60bc743073c66e073b774
Boris Panchenko <oberon@list.ru>
2024-05-27 19:45:03 +0300

    Impl `Palette` class

--- index.d.ts
+++ index.d.ts
@@ -5,68 +5,90 @@ type CamelCase<S extends string> = S extends `${infer P1}-${infer P2}`
 	? `${Lowercase<P1>}${Capitalize<P2>}`
 	: Lowercase<S>
 
-export type AccentKey = ArrayValues<typeof accentKeys>
-export type AccentCorFn = (
-	index: ArrayIndices<typeof accentKeys>
-) => [number, number]
-export type ToneCorFn = (index: ArrayIndices<typeof toneKeys>) => number
-export type ToneKey = ArrayValues<typeof toneKeys>
+declare global {
+	type ColorKey =
+		| 'red'
+		| 'pink'
+		| 'purple'
+		| 'violet'
+		| 'indigo'
+		| 'blue'
+		| 'light-blue'
+		| 'cyan'
+		| 'teal'
+		| 'green'
+		| 'light-green'
+		| 'lime'
+		| 'yellow'
+		| 'amber'
+		| 'orange'
+		| 'deep-orange'
+		| 'brown'
+		| 'gray'
 
-export type ColorKey =
-	| 'red'
-	| 'pink'
-	| 'purple'
-	| 'violet'
-	| 'indigo'
-	| 'blue'
-	| 'light-blue'
-	| 'cyan'
-	| 'teal'
-	| 'green'
-	| 'light-green'
-	| 'lime'
-	| 'yellow'
-	| 'amber'
-	| 'orange'
-	| 'deep-orange'
-	| 'brown'
-	| 'gray'
+	type AccentKey = ArrayValues<typeof accentKeys>
+	type AccentCorFn = (
+		index: ArrayIndices<typeof accentKeys>
+	) => [number, number]
+	type ToneCorFn = (index: ArrayIndices<typeof toneKeys>) => number
+	type ToneKey = ArrayValues<typeof toneKeys>
 
-export type ColorName = {
-	[K in ColorKey]: K extends 'gray'
-		? CamelCase<K>
-		: CamelCase<K> | CamelCase<`${K}-gray`>
-}[ColorKey]
+	type ColorKey =
+		| 'red'
+		| 'pink'
+		| 'purple'
+		| 'violet'
+		| 'indigo'
+		| 'blue'
+		| 'light-blue'
+		| 'cyan'
+		| 'teal'
+		| 'green'
+		| 'light-green'
+		| 'lime'
+		| 'yellow'
+		| 'amber'
+		| 'orange'
+		| 'deep-orange'
+		| 'brown'
+		| 'gray'
 
-export type ColorData = {
-	hsl: ColorValue
-	hex: ColorValue
-	rgb: ColorValue
-}
+	type ColorName = {
+		[K in ColorKey]: K extends 'gray'
+			? CamelCase<K>
+			: CamelCase<K> | CamelCase<`${K}-gray`>
+	}[ColorKey]
 
-export type ColorValue = string
-
-export interface Config {
-	admixturesToGray?: ColorKey
-	colors: Record<ColorKey, ColorValue>
-	correlations: {
-		accent: AccentCorFn | [number, number, number, number]
-		tone: ToneCorFn | [number, number, number, number]
+	type ColorData = {
+		hsl: ColorValue
+		hex: ColorValue
+		rgb: ColorValue
 	}
-	name: string
-	theme: 'dark' | 'light' | ThemeConfig
-}
 
-export interface SourceConfig
-	extends Required<Pick<Config, 'colors' | 'correlations'>>,
-		Optional<Omit<Config, 'colors' | 'correlations'>> {}
+	type ColorValue = string
 
-export type PaletteColorData = ColorData & {
-	accents?: Record<AccentKey, ColorData>
-	tones?: Record<ToneKey, ColorData>
-}
+	interface Config {
+		admixturesToGray?: ColorKey
+		colors: Record<ColorKey, ColorValue>
+		correlations: {
+			accent: AccentCorFn | [number, number, number, number]
+			tone: ToneCorFn | [number, number, number, number]
+		}
+		name: string
+		theme: 'dark' | 'light' | ThemeConfig
+	}
 
-export type ThemeConfig = {
-	background: ColorValue
-	foreground: ColorValue
+	interface SourceConfig
+		extends Required<Pick<Config, 'colors' | 'correlations'>>,
+			Optional<Omit<Config, 'colors' | 'correlations'>> {}
+
+	type PaletteColorData = ColorData & {
+		accents?: Record<AccentKey, ColorData>
+		tones?: Record<ToneKey, ColorData>
+	}
+
+	type ThemeConfig = {
+		background: ColorValue
+		foreground: ColorValue
+	}
 }
