c183a6e680d009881330a0fd511e12facceb3aa4
Boris Vasilievich Panchenko <oberon@list.ru>
2025-06-20 11:21:13 +0300

    new: Support for Android Resources file (.xml)

--- builder/index.js
+++ builder/index.js
@@ -1,7 +1,7 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 
-import { makeColorDTO } from 'chromatic-chandelier/toolkit.js';
 import Handlebars from "handlebars";
+import { makeColorDTO } from '../../Constructor.Package/.public/toolkit.js';
 
 import dataset from "../dataset.json" with { type: 'json' };
 import packageJSON from "../package.json" with { type: 'json' };
@@ -15,26 +15,32 @@ for (let idx = 0; idx < length; idx++) {
 	const { ident, value } = dataset.data[idx]
 	colorset[idx] = await makeColorDTO(value, ident)
 	colorset[idx]['digihex'] = colorset[idx]['hex'].replace('#', '0x')
+	colorset[idx]['underscored-name'] = colorset[idx]['css-name'].replace('--', '').replace(/-+/g, '_')
 }
 
 const tpl = {
 	'css-stylesheet': readFileSync('template/css.stylesheet.hbs', 'utf-8'),
 	'esmodule': readFileSync('template/js.esmodule.hbs', 'utf-8'),
+	'xml': readFileSync('template/xml.android-recources.hbs', 'utf-8'),
 	'setup-css-props': readFileSync('template/js.setup-css-props.hbs', 'utf-8'),
 }
 
 const entryPoints = [
 	['css.stylesheet', {
 		template: Handlebars.compile(tpl['css-stylesheet']),
-		outfile: '.bundle/lib.protosite-palette.css'
+		outfile: '.bundle/protosite-palette.css'
+	}],
+	['js.esmodule', {
+		template: Handlebars.compile(tpl['esmodule']),
+		outfile: '.bundle/protosite-palette.mjs'
+	}],
+	['xml.android-recources', {
+		template: Handlebars.compile(tpl['xml']),
+		outfile: '.bundle/protosite-palette.xml'
 	}],
 	['js.setup-css-props', {
 		template:Handlebars.compile(tpl['setup-css-props']),
 		outfile: '.bundle/setup.protosite-palette.js'
-	}],
-	['js.esmodule', {
-		template: Handlebars.compile(tpl['esmodule']),
-		outfile: '.bundle/lib.protosite-palette.mjs'
 	}],
 ];
 
